698d6d6e18c139a19373d596465e2894d4424044
disstian <as01788@163.com>
2022-03-14 18:52:55 +0800

    .

--- source/panel/operation/nodes/procedural/noise/VoronoiNode.ts
+++ source/panel/operation/nodes/procedural/noise/VoronoiNode.ts
@@ -1,6 +1,6 @@
 import { ShaderNode } from "../../../base";
 
-export default class VoronoiNode extends ShaderNode{
+export default class VoronoiNode extends ShaderNode {
 
     generateCode(): string {
         let uv = this.getInputValue(0);
@@ -13,20 +13,24 @@ export default class VoronoiNode extends ShaderNode{
             vec2 f = frac(uvc);
             float t = 8.0;
             vec3 res = vec3(8.0,.0,.0);
-            for(int y=-1;y<=1;y++){
-                for(int x=-1;x<=1;x++){
+            float x = .0;
+            float y = .0;
+            for(float y=-1.;y<=1.;y++){
+                for(float x=-1.;x<=1.;x++){
                     vec2 lattice = vec2(x,y);
 
-                    vec4 m = vec4(15.27,47.63,99.41,89.98);
-                    float frac1 = sin(mul(lattice+g,m))*46839.32;
-                    lattice = frac(frac1);
+                    mat2 m = mat2(15.27,47.63,99.41,89.98);
+                    vec2 o = (lattice+g) * m;
+                    float f1 = sin(o.x);
+                    float f2 = sin(o.y);
+                    lattice = frac(vec2(f1,f2)*46839.32);
 
                     vec2 offset = vec2(sin(lattice.y*+${angle})*0.5+0.5,cos(lattice.x*${angle})*0.5+0.5);
                     float d = distance(lattice + ${angle},f);
                     if(d < res.x){
                         res = vec3(d,offset.x,offset.y);
-                        ${this.getOutputVarDefine(0)} = res.x;
-                        ${this.getOutputVarDefine(1)} = res.y;
+                        x = res.x;
+                        y = res.y;
                     }
                 }
             }
